d1b655e40f38205b73e4d11db8c4a7352b9ad7b7
ArturZybin <artur.zybin.work@gmail.com>
2020-03-25 11:55:28 +0700

    Updates

---
+++ precache-manifest.3aed2f79ba3b9430192056e7036f3448.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85dd1da4ba5f9c622fb80ce05c187390",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72d8495dae25e725c1af",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "61f270ea73fba96bf838",
+    "url": "./static/css/main.3ae1fa49.chunk.css"
+  },
+  {
+    "revision": "72d8495dae25e725c1af",
+    "url": "./static/js/2.3bf1f976.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3bf1f976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f270ea73fba96bf838",
+    "url": "./static/js/main.6a0fbc44.chunk.js"
+  },
+  {
+    "revision": "98490d1e15b342bb8939",
+    "url": "./static/js/runtime-main.316c05fd.js"
+  },
+  {
+    "revision": "2dcbf8cfe65fce2b5fefd68fc1ad5239",
+    "url": "./static/media/bag.2dcbf8cf.svg"
+  },
+  {
+    "revision": "7d3288e50addbae56eba73364b7ae0ac",
+    "url": "./static/media/filled-bag.7d3288e5.svg"
+  },
+  {
+    "revision": "0f0a056e8d5201f34189fc50f0566f97",
+    "url": "./static/media/filled-heart.0f0a056e.svg"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "./static/media/heart.7746f5e4.svg"
+  },
+  {
+    "revision": "3d261bb2fd2c3b51b0ff7ea309832f03",
+    "url": "./static/media/title.3d261bb2.svg"
+  }
+]);
