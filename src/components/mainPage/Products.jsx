cb52d10afe29a1275a21c0c82920e9aaaec86143
ArturZybin <artur.zybin.work@gmail.com>
2020-03-25 19:16:47 +0700

    add loading element to main page

--- src/components/mainPage/Products.jsx
+++ src/components/mainPage/Products.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+
 import { fetchFirstProducts, fetchNewProducts } from '../../redux/actions';
 import { Product } from './Product';
+import { Loading } from './Loading';
 
 
 export function Products() {
@@ -10,26 +12,29 @@ export function Products() {
    const filters = useSelector((state) => state.mainPage.filters)
    let products = useSelector((state) => state.products);
    const nextId = (products.length) ? products[products.length - 1].id + 1 : null
-   
+
    if (!products.length && !isLoading) {
       dispatch(fetchFirstProducts())
    }
-   
+
    products = products.filter((product) => filterProduct(product, filters))
 
    products = products.map((product) => (
       <Product product={product} key={product.id} />
    ));
-   
-   
+
+
    return (
-      <div className="main-page__products">
-         { products }
-         <button
-            className="main-page__load-more-button"
-            onClick={() => dispatch(fetchNewProducts(nextId))}
-         >Load more</button>
-      </div>
+      products.length ?
+         <div className="main-page__products">
+            {products}
+            <button
+               className="main-page__load-more-button"
+               onClick={() => dispatch(fetchNewProducts(nextId))}
+            >Load more</button>
+         </div>
+         :
+         <Loading />
    )
 }
 
